5ec59149398d486b7b0349b1b1a27bd1838a0ff3
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-06-18 11:57:23 -0300

    added poo

---
+++ oop/aula_1/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa{
+    public static void main(String args[]){
+        int cpf = 0;
+        String nome = "";
+
+        cpf = 104;
+        nome = "Julio";
+
+        System.out.println("Nome: " + nome);
+        System.out.printf("CPF: %d\n", cpf);
+    }
+}
