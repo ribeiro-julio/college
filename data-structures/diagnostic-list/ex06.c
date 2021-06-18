c868c9c0820e109ad51d7fc63e837dc7dd319605
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-06-18 11:57:36 -0300

    fixed errors

--- data-structures/diagnostic-list/ex06.c
+++ data-structures/diagnostic-list/ex06.c
@@ -42,60 +42,110 @@ int non_dealt_cards(Deck *deck){
 }
 
 Card* on_top(Deck *deck){
+    if(non_dealt_cards(deck) == 0)
+        return NULL;
+
     for(int i = 53; i >= 0; i--){
         if(!deck->cards[i].was_dealt)
             return &deck->cards[i];
     }
-
-    printf("Nao existem mais cartas no baralho");
-    return NULL;
 }
 
 Card* on_bottom(Deck *deck){
-    for(int i = 0; i >= 53; i++){
+    if(non_dealt_cards(deck) == 0)
+        return NULL;
+
+    for(int i = 0; i < 53; i++){
         if(!deck->cards[i].was_dealt)
             return &deck->cards[i];
     }
-
-    printf("Nao existem mais cartas no baralho");
-    return NULL;
 }
 
 Card* deal_hand(Deck *deck){
+    if(non_dealt_cards(deck) <= 2)
+        return NULL;
+    
+    if(non_dealt_cards(deck) == 3){
+        Card *hand = malloc(sizeof(Card)*3);
+
+        int index = 0;
+        for(int i = 0; i < 54; i++){
+            if(!deck->cards[i].was_dealt){
+                hand[index] = deck->cards[i];
+                deck->cards[i].was_dealt = true;
+                index++;
+            }
+        }
+
+        return hand;
+    }
+
     srand(time(NULL));
 
-    Card hand[3];
+    Card *hand = malloc(sizeof(Card)*3);
     int r;
     for(int i = 0; i < 3; i++){
         r = rand() % 53;
         while(deck->cards[r].was_dealt)
             r = rand() % 53;
-        hand[i] = deck->cards[i];
-        deck->cards[i].was_dealt = true;
+        hand[i] = deck->cards[r];
+        deck->cards[r].was_dealt = true;
     }
 
     return hand;
 }
 
 void print_card(Card *card){
-    printf("%c de %c\n", &card->value, &card->suit);
+    if(card == NULL)
+        return;
+    
+    if(card->value == 'C')
+        printf("Coringa\n");
+    else{
+        if(card->value == 'D')
+            printf("Dez de ");
+        else
+            printf("%c de ", card->value);
+        switch(card->suit){
+            case 'P': printf("Paus\n"); return;
+            case 'E': printf("Espadas\n"); return;
+            case 'C': printf("Copas\n"); return;
+            case 'O': printf("Ouros\n"); return;
+        }
+    }
 }
 
 int main(){
-    Deck *deck;
+    Deck deck;
+    Card *hand;
 
-    create_deck(deck);
-    printf("Quantidade de cartas no baralho: %d\n", non_dealt_cards(deck));
-    printf("Carta no topo:\n");
-    print_card(on_top(deck));
-    printf("---------------------------------------\n");
-    printf("Carta no fundo:\n");
-    print_card(on_bottom(deck));
-    printf("---------------------------------------\n");
-    Card *hand = deal_hand(deck);
-    printf("Carteado:\n");
-    //print_card(hand[0]);
-    //print_card(hand[1]);
-    //print_card(hand[2]);
+    create_deck(&deck);
+
+    while(non_dealt_cards(&deck) != 0 && hand != NULL){
+        printf("Quantidade de cartas no baralho: %d\n", non_dealt_cards(&deck));
+
+        printf("Carta no topo:\n");
+        if(on_top(&deck) != NULL)
+            print_card(on_top(&deck));
+        else
+            printf("Nao existem mais cartas no baralho\n");
+        printf("---------------------------------------\n");
+
+        printf("Carta no fundo:\n");
+        if(on_bottom(&deck) != NULL)
+            print_card(on_bottom(&deck));
+        else
+            printf("Nao existem mais cartas no baralho\n");
+        printf("---------------------------------------\n");
+
+        hand = deal_hand(&deck);
+        if(hand != NULL){
+            printf("Carteado:\n");
+            print_card(hand);
+            print_card(hand+1);
+            print_card(hand+2);
+        }
+    }
+    
     return 0;
 }
