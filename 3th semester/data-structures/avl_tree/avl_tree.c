9ff992a6e88d15dbab852bdf277a3bb16b29a75d
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-08-10 23:02:44 -0300

    finished insert

--- 3th semester/data-structures/avl_tree/avl_tree.c
+++ 3th semester/data-structures/avl_tree/avl_tree.c
@@ -26,25 +26,25 @@ void preOrder(AVLNodePtr *node) {
         return;
 
     printf("%d ", (*node)->x);
-    preOrder((*node)->left);
-    preOrder((*node)->right);
+    preOrder(&(*node)->left);
+    preOrder(&(*node)->right);
 }
 
 void inOrder(AVLNodePtr *node) {
     if((*node) == NULL)
         return;
 
-    inOrder((*node)->left);
+    inOrder(&(*node)->left);
     printf("%d ", (*node)->x);
-    inOrder((*node)->right);
+    inOrder(&(*node)->right);
 }
 
 void postOrder(AVLNodePtr *node) {
     if((*node) == NULL)
         return;
 
-    postOrder((*node)->left);
-    postOrder((*node)->right);
+    postOrder(&(*node)->left);
+    postOrder(&(*node)->right);
     printf("%d ", (*node)->x);
 }
 
@@ -56,9 +56,9 @@ bool search(AVLNodePtr *node, int x) {
         return true;
     
     if(x > (*node)->x)
-        return search((*node)->right, x);
+        return search(&(*node)->right, x);
     else
-        return search((*node)->right, x);
+        return search(&(*node)->right, x);
 }
 
 void destroy(AVLNodePtr *node) {
@@ -67,6 +67,108 @@ void destroy(AVLNodePtr *node) {
         destroy(&(*node)->right);
         free((*node));
         (*node) = NULL;
+    }
+}
+
+int AVLHeight(AVLNodePtr node) {
+    if(node == NULL)
+        return 0;
+    else
+        return node->height;
+}
+
+int updateAVLHeight(AVLNodePtr left, AVLNodePtr right) {
+    int lh = AVLHeight(left);
+    int rh = AVLHeight(right);
+
+    if(lh > rh)
+        return (lh + 1);
+    else
+        return (rh + 1);
+}
+
+void rightSimpleRotation(AVLNodePtr *p) {
+    AVLNodePtr u = (*p)->left;
+
+    (*p)->left = u->right;
+    u->right = (*p);
+    (*p)->height = 0;
+
+    (*p)->height = updateAVLHeight((*p)->left, (*p)->right);
+    u->height = updateAVLHeight(u->left, u->right);
+
+    (*p) = u;
+}
+
+void leftSimpleRotation(AVLNodePtr *p) {
+    AVLNodePtr u = (*p)->right;
+
+    (*p)->right = u->left;
+    u->left = (*p);
+    (*p)->height = 0;
+
+    (*p)->height = updateAVLHeight((*p)->left, (*p)->right);
+    u->height = updateAVLHeight(u->left, u->right);
+
+    (*p) = u;
+}
+
+void rightDoubleRotation(AVLNodePtr *p) {
+    AVLNodePtr u = (*p)->left;
+    AVLNodePtr v = u->right;
+
+    (*p)->left = v->right;
+    u->right = v->left;
+    v->right = (*p);
+    v->left = u;
+
+    (*p)->height = updateAVLHeight((*p)->left, (*p)->right);
+    u->height = updateAVLHeight(u->left, u->right);
+    v->height = updateAVLHeight(v->left, v->right);
+
+    (*p) = v;
+}
+
+void leftDoubleRotation(AVLNodePtr *p) {
+    AVLNodePtr u = (*p)->right;
+    AVLNodePtr v = u->left;
+
+    (*p)->right = v->left;
+    u->left = v->right;
+    v->left = (*p);
+    v->right = u;
+
+    (*p)->height = updateAVLHeight((*p)->left, (*p)->right);
+    u->height = updateAVLHeight(u->left, u->right);
+    v->height = updateAVLHeight(v->left, v->right);
+
+    (*p) = v;
+}
+
+void applyRotations(AVLNodePtr *node) {
+    int rh = AVLHeight((*node)->right);
+    int lh = AVLHeight((*node)->left);
+    int bf = rh - lh;
+
+    if(bf == 2) {   // Rotaciona pra esquerda -> mais elementos na direita
+        AVLNodePtr temp = (*node)->right;
+        int temprh = AVLHeight(temp->right);
+        int templh = AVLHeight(temp->left);
+
+        if(temprh >= templh)  // Rotacao simples
+            leftSimpleRotation(&(*node));
+        else  // rotação dupla
+            leftDoubleRotation(&(*node));
+
+    } else {        // Rotaciona pra direita ->  mais elementos na esquerda
+        AVLNodePtr temp = (*node)->left;
+        int temprh = AVLHeight(temp->right);
+        int templh = AVLHeight(temp->left);
+
+        if(templh >= temprh)  // Rotacao simples
+            rightSimpleRotation(&(*node));
+        else  // rotação dupla
+            rightDoubleRotation(&(*node));
     }
 }
 
@@ -93,57 +195,15 @@ bool insert(AVLNodePtr *node, int x) {
     if(inserted == false)
         return false;
     
-    int leftHeight, rightHeight;
-    leftHeight = AVLHeight((*node)->left);
-    rightHeight = AVLHeight((*node)->right);
+    int rh = AVLHeight((*node)->right);
+    int lh = AVLHeight((*node)->left);
 
-    if((rightHeight - leftHeight) == 2 || (rightHeight - leftHeight) == -2)
-        applyRotations(node);
+    if((rh - lh) == 2 || (rh - lh) == -2)
+        applyRotations(&(*node));
 
     (*node)->height = updateAVLHeight((*node)->left, (*node)->right);
-}
 
-int AVLHeight(AVLNodePtr node) {
-    if(node == NULL)
-        return 0;
-    else
-        return node->height;
-}
-
-int updateAVLHeight(AVLNodePtr left, AVLNodePtr right) {
-    int lt = AVLHeight(left);
-    int rt = AVLHeight(right);
-
-    if(lt > rt)
-        return (lt + 1);
-    else
-        return (rt + 1);
-}
-
-void applyRotations(AVLNodePtr *node) {
-    int lt = AVLHeight((*node)->left);
-    int rt = AVLHeight((*node)->right);
-    int bf = rt - lt;
-
-    if(bf == 2) {   // Rotaciona pra esquerda
-        AVLNodePtr temp = (*node)->right;
-        int temprh = AVLHeight(temp->right);
-        int templh = AVLHeight(temp->left);
-
-        if(templh > temprh)    // Rotacao simples
-            leftDoubleRotation(&(*node));
-        else  // rotação dupla
-            leftSimpleRotation(&(*node));
-    } else {        // Rotaciona pra direita
-        AVLNodePtr temp = (*node)->left;
-        int temprh = AVLHeight(temp->right);
-        int templh = AVLHeight(temp->left);
-
-        if(temprh > templh)    // Rotacao simples
-            rightDoubleRotation(&(*node));
-        else    // rotação dupla
-            rightSimpleRotation(&(*node));
-    }
+    return true;
 }
 
 
@@ -155,7 +215,26 @@ int main() {
     if(isEmpty(&AVLRoot))
         printf("Arvore vazia\n");
 
+    insert(&AVLRoot, 77);
+    insert(&AVLRoot, 12);
+    insert(&AVLRoot, 10);
+    insert(&AVLRoot, 5);
+    insert(&AVLRoot, 7);
+    insert(&AVLRoot, 80);
+    insert(&AVLRoot, 78);
+    insert(&AVLRoot, 90);
+    insert(&AVLRoot, 100);
+    insert(&AVLRoot, 2);
+    insert(&AVLRoot, 1);
+
+    printf("PreOrder = { ");
+    preOrder(&AVLRoot);
+    printf("}\n");
+
     destroy(&AVLRoot);
+
+    if(isEmpty(&AVLRoot))
+        printf("Arvore vazia\n");
 
     return 0;
 }
