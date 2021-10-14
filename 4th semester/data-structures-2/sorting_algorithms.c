cde3d83350dd7c7ad9ac4aef2c6a975cccd3bf36
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-10-14 17:40:33 -0300

    add selection and insertion sort

--- 4th semester/data-structures-2/sorting_algorithms.c
+++ 4th semester/data-structures-2/sorting_algorithms.c
@@ -16,12 +16,42 @@ void bubble_sort(int array[], int n) {
     }
 }
 
+void selection_sort(int array[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        int min = array[i];
+        int i_min;
+        for(int j = i+1; j < n; j++) {
+            if(array[j] < min) {
+                i_min = j;
+                min = array[j];
+            }
+        }
+        if(min != array[i]) {
+            int aux = array[i];
+            array[i] = min;
+            array[i_min] = aux;
+        }
+    }
+}
+
+void insertion_sort(int array[], int n) {
+    for(int i = 1; i < n; i++) {
+        int aux = array[i];
+        int j = i-1;
+        while(j >= 0 && aux < array[j]) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = aux;
+    }
+}
+
 
 int main() {
     int rand[] = { 35, 15, 36, 13, 1, 40, 11, 17, 37, 33 };
     int decres[] = { 40, 37, 36, 35, 33, 17, 15, 13, 11, 1 };
 
-    bubble_sort(rand, sizeof(rand)/sizeof(rand[0]));
+    insertion_sort(rand, sizeof(rand)/sizeof(rand[0]));
     bubble_sort(decres, sizeof(decres)/sizeof(decres[0]));
 
     printf("Vetor randomico organizado: {");
