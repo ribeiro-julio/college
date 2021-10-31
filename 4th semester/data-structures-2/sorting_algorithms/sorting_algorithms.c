67087d6a942f1750d205dc4c675884ea70078809
Julio Ribeiro <julio.ribeiro303@gmail.com>
2021-10-31 16:36:37 -0300

    add upgrades
    - fix input arrays
    - fix comparison variable types
    - add comparison counter for merge and quick sorts
    - implement output for merge and quick sorts
    - add comments

--- 4th semester/data-structures-2/sorting_algorithms/sorting_algorithms.c
+++ 4th semester/data-structures-2/sorting_algorithms/sorting_algorithms.c
@@ -1,3 +1,5 @@
+// JULIO CESAR GARCIA RIBEIRO   RA: 1994484
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,13 +7,16 @@
 #include <string.h>
 #include <time.h>
 
-int bubble_sort(int array[], int n) {
-    int comparisons = 0;
+// ----------------------Bubble Sort----------------------
+
+unsigned long bubble_sort(int array[], int n) {
+    unsigned long comparisons = 0;
 
     bool changed = true;
     while(changed) {
         changed = false;
         for(int i = 0; i < n-1; i++) {
+            // Cada vez que entra no segundo for acontece uma comparação entre os elementos do vetor
             comparisons++;
             if(array[i] > array[i+1]) {
                 int aux = array[i];
@@ -25,21 +30,24 @@ int bubble_sort(int array[], int n) {
     return comparisons;
 }
 
-// -------------------------------------------------------
+// ----------------------Selection Sort----------------------
 
-int selection_sort(int array[], int n) {
-    int comparisons = 0;
+unsigned long selection_sort(int array[], int n) {
+    unsigned long comparisons = 0;
 
     for(int i = 0; i < n-1; i++) {
         int min = array[i];
         int i_min;
         for(int j = i+1; j < n; j++) {
+            // Cada vez que entra no segundo for acontece uma comparação entre os elementos do vetor
             comparisons++;
             if(array[j] < min) {
                 i_min = j;
                 min = array[j];
             }
         }
+        // Adição de mais uma comparação para saber se o menor valor já não está em sua posição correta
+        comparisons++;
         if(min != array[i]) {
             int aux = array[i];
             array[i] = min;
@@ -50,15 +58,16 @@ int selection_sort(int array[], int n) {
     return comparisons;
 }
 
-// -------------------------------------------------------
+// ----------------------Insertion Sort----------------------
 
-int insertion_sort(int array[], int n) {
-    int comparisons = 0;
+unsigned long insertion_sort(int array[], int n) {
+    unsigned long comparisons = 0;
 
     for(int i = 1; i < n; i++) {
         int aux = array[i];
         int j = i-1;
         while(j >= 0 && aux < array[j]) {
+            // Cada execução do while acontece uma comparação entre os elementos do vetor
             comparisons++;
             array[j+1] = array[j];
             j--;
@@ -69,9 +78,9 @@ int insertion_sort(int array[], int n) {
     return comparisons;
 }
 
-// -------------------------------------------------------
+// ----------------------Merge Sort----------------------
 
-void merge(int array[], int start, int middle, int end) {
+void merge(int array[], int start, int middle, int end, unsigned long *comparisons) {
     int i, j, k;
 
     int p1 = start;
@@ -83,6 +92,8 @@ void merge(int array[], int start, int middle, int end) {
     if(aux != NULL) {
         for(int i = 0; i < aux_size; i++) {
             if(p1 <= middle && p2 <= end) {
+                // Única comparação entre os elementos do vetor
+                *comparisons = *comparisons + 1;
                 if(array[p1] < array[p2])
                     aux[i] = array[p1++];
                 else
@@ -102,27 +113,33 @@ void merge(int array[], int start, int middle, int end) {
     free(aux);
 }
 
-void merge_sort(int array[], int start, int end) {
+int merge_sort(int array[], int start, int end, unsigned long *comparisons) {
     if(start < end) {
         int middle = (int)floor((start + end)/2);
-        merge_sort(array, start, middle);
-        merge_sort(array, middle + 1, end);
-        merge(array, start, middle, end);
+        merge_sort(array, start, middle, comparisons);
+        merge_sort(array, middle + 1, end, comparisons);
+        merge(array, start, middle, end, comparisons);
     }
 }
 
-// -------------------------------------------------------
+// ----------------------Quick Sort----------------------
 
-int part(int array[], int start, int end) {
+int part(int array[], int start, int end, unsigned long *comparisons) {
     int left = start;
     int right = end;
     int pivot = array[start];
 
     while(left < right) {
-        while(array[left] <= pivot && left <= end)
+        while(array[left] <= pivot && left <= end) {
+            // Comparações para encontrar o elemento maior que o pivo
+            *comparisons = *comparisons + 1;
             left++;
-        while(array[right] > pivot && right >= start)
+        }
+        while(array[right] > pivot && right >= start) {
+            // Comparações para encontrar o elemento menor que o pivo
+            *comparisons = *comparisons + 1;
             right--;
+        }
         if(left < right) {
             int aux = array[right];
             array[right] = array[left];
@@ -136,32 +153,37 @@ int part(int array[], int start, int end) {
     return right;
 }
 
-void quick_sort(int array[], int start, int end) {
+void quick_sort(int array[], int start, int end, unsigned long *comparisons) {
     if(start < end) {
-        int i_pivot = part(array, start, end);
-        quick_sort(array, start, i_pivot-1);
-        quick_sort(array, i_pivot+1, end);
+        int i_pivot = part(array, start, end, comparisons);
+        quick_sort(array, start, i_pivot-1, comparisons);
+        quick_sort(array, i_pivot+1, end, comparisons);
     }
 }
 
-// -------------------------------------------------------
+// ----------------------Imprimir Array----------------------
 
 void printArray(FILE *file, int *array, int n) {
     for(int i = 0; i < n; i++)
         fprintf(file, " %d", array[i]);
 }
 
+// ----------------------Receber Valores do Arquivo de Entrada----------------------
+
 bool getParams(FILE *file, int *n, char *method) {
     char buffer[13];
 
+    // Lê primeira linha do arquivo
     fgets(buffer, 12, file);
     buffer[strcspn(buffer, "\r")] = 0;
     buffer[strcspn(buffer, "\n")] = 0;
     
     *n = atoi(buffer);
+    // Se a primera linha for um número não irá retornar zero (também filtra se n for 0 (vetor fazio))
     if(*n == 0)
         return false;
 
+    // Se o proximo caractere, tem que ser c, d, ou r, caso contrário o arquivo é inválido
     *method = fgetc(file);
     if(*method != 'c' && *method != 'd' && *method != 'r') {
         return false;
@@ -170,17 +192,24 @@ bool getParams(FILE *file, int *n, char *method) {
     return true;
 }
 
+// ----------------------Escrever Arquivo de Saída----------------------
+
 bool fillOutput(FILE *file, int bubble[], int selection[], int insertion[], int merge[], int quick[], int n) {
-    int comparisons;
+    unsigned long comparisons;
     clock_t start_time;
     int time_taken_millis;
+
+    // Para os métodos simples:
+    //  - Inicia o clock, chama o método que retorna o número de comparações, calcula o tempo necessário, printa o vetor, quantidade de comparações e tempo necessário
+    // Para os métodos recursivos:
+    //  - Inicia o clock, zera variável de comparações e a passa como referência para o método, chama o método, calcula o tempo necessário, printa o vetor, quantidade de comparações e tempo necessário
 
     fprintf(file, "insertionSort:");
     start_time = clock();
     comparisons = insertion_sort(insertion, n);
     time_taken_millis = (int)((clock()-start_time)*1E3/CLOCKS_PER_SEC);
     printArray(file, insertion, n);
-    fprintf(file, ", %d comp", comparisons);
+    fprintf(file, ", %lu comp", comparisons);
     fprintf(file, ", %d ms\n", time_taken_millis);
 
     fprintf(file, "selectionSort:");
@@ -188,7 +217,7 @@ bool fillOutput(FILE *file, int bubble[], int selection[], int insertion[], int 
     comparisons = selection_sort(selection, n);
     time_taken_millis = (int)((clock()-start_time)*1E3/CLOCKS_PER_SEC);
     printArray(file, selection, n);
-    fprintf(file, ", %d comp", comparisons);
+    fprintf(file, ", %lu comp", comparisons);
     fprintf(file, ", %d ms\n", time_taken_millis);
 
     fprintf(file, "bubbleSort:");
@@ -196,28 +225,42 @@ bool fillOutput(FILE *file, int bubble[], int selection[], int insertion[], int 
     comparisons = bubble_sort(bubble, n);
     time_taken_millis = (int)((clock()-start_time)*1E3/CLOCKS_PER_SEC);
     printArray(file, bubble, n);
-    fprintf(file, ", %d comp", comparisons);
+    fprintf(file, ", %lu comp", comparisons);
     fprintf(file, ", %d ms\n", time_taken_millis);
 
-    //fprintf(file, "mergeSort:");
+    fprintf(file, "mergeSort:");
+    start_time = clock();
+    comparisons = 0;
+    merge_sort(merge, 0, n-1, &comparisons);
+    time_taken_millis = (int)((clock()-start_time)*1E3/CLOCKS_PER_SEC);
+    printArray(file, merge, n);
+    fprintf(file, ", %lu comp", comparisons);
+    fprintf(file, ", %d ms\n", time_taken_millis);
 
-    //fprintf(file, "quickSort:");
+    fprintf(file, "quickSort:");
+    start_time = clock();
+    comparisons = 0;
+    quick_sort(quick, 0, n-1, &comparisons);
+    time_taken_millis = (int)((clock()-start_time)*1E3/CLOCKS_PER_SEC);
+    printArray(file, quick, n);
+    fprintf(file, ", %lu comp", comparisons);
+    fprintf(file, ", %d ms", time_taken_millis);
 }
+
+// ----------------------Main----------------------
 
 int main(int argc, char *argv[]) {
     if(argc != 3) {
         printf("O programa deve receber 3 parametros [nome_do_programa] [arquivo_de_entrada] [arquivo_de_saida]\n");
-        //exit(1);
+        exit(1);
     }
 
-    //FILE *input = fopen(argv[1], "r");
-    FILE *input = fopen("./input/input3.txt", "r");
+    FILE *input = fopen(argv[1], "r");
     if(input == NULL) {
         printf("Erro ao abrir o arquivo de entrada\n");
         exit(2);
     }
-    //FILE *output = fopen(argv[2], "w");
-    FILE *output = fopen("./output/output3.txt", "w");
+    FILE *output = fopen(argv[2], "w");
     if(output == NULL) {
         printf("Erro ao abrir o arquivo de saida\n");
         exit(2);
@@ -225,36 +268,54 @@ int main(int argc, char *argv[]) {
 
     int n;
     char method;
-
+    
+    // Pega os dados necessários do arquivo de entrada (se o arquivo de entrada for válido continua, senão mata o programa)
     if(getParams(input, &n, &method)) {
-        int input_array[n];
+        // Cria um vetor para cada método
+        int bubble[n];
+        int selection[n];
+        int insertion[n];
+        int merge[n];
+        int quick[n];
 
+        // Preenche os vetores com os valores requeridos
         if(method == 'c') {
-            for(int i = 1; i <= n; i++)
-                input_array[i-1] = i;
+            for(int i = 1; i <= n; i++) {
+                bubble[i-1] = i;
+                selection[i-1] = i;
+                insertion[i-1] = i;
+                merge[i-1] = i;
+                quick[i-1] = i;
+            }
         } else if (method == 'd') {
             int aux = n;
             for(int i = 0; i < n; i++) {
-                input_array[i] = aux;
+                bubble[i] = aux;
+                selection[i] = aux;
+                insertion[i] = aux;
+                merge[i] = aux;
+                quick[i] = aux;
+
                 aux--;
             }
         } else if (method == 'r') {
             srand(time(NULL));
             for(int i = 0; i < n; i++) {
-                input_array[i] = (rand() % 32001);
+                int aux = (rand() % 32001);
+
+                bubble[i] = aux;
+                selection[i] = aux;
+                insertion[i] = aux;
+                merge[i] = aux;
+                quick[i] = aux;
             }
         }
-
-        int *bubble = input_array;
-        int *selection = input_array;
-        int *insertion = input_array;
-        int *merge = input_array;
-        int *quick = input_array;
 
         fillOutput(output, bubble, selection, insertion, merge, quick, n);
     } else
         fprintf(output, "Arquivo inválido!");
 
+    // Fecha os arquivos
     fclose(input);
     fclose(output);
 
